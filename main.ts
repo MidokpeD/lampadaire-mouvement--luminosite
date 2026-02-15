80892192f47fe692e7d20358b56712085d9c8f08
MidokpeD <160834660+MidokpeD@users.noreply.github.com>
2026-02-15 14:16:39 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,17 @@
+let Luminosité = 0
 let Mouvement = 0
 basic.forever(function () {
+    Luminosité = pins.analogReadPin(AnalogReadWritePin.P1)
     Mouvement = pins.digitalReadPin(DigitalPin.P11)
-    if (Mouvement == 1) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.showLeds(`
-            . . . . #
-            . # # # #
-            . # # # .
-            . # # # .
-            # # . . .
-            `)
-        basic.pause(3000)
-        pins.digitalWritePin(DigitalPin.P2, 1)
+    if (Luminosité < 500) {
+        if (Mouvement == 1) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(3000)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+        } else {
+            pins.digitalWritePin(DigitalPin.P2, 0)
+        }
     } else {
         pins.digitalWritePin(DigitalPin.P2, 0)
     }
-})
-basic.forever(function () {
-	
 })
